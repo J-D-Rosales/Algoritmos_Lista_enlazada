78a220df2f78bfb31acb42dda99c0fc2cde3374b
Elmer Villegas <elmer.villegas@utec.edu.pe>
2025-08-31 11:18:27 -0500

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -20,116 +20,10 @@ class List {
     public:
     List() : head(nullptr) {}
 
-    Node* getHead() { return head; }
-
-    void pushBack(int numerito) {
-        Node* nodo = new Node(numerito);
-        if (!head) { head = nodo; return; }
-        Node* temp = head;
-        while (temp->next != NULL) {
-            temp = temp->next;
-        }
-        temp->next = nodo;
-        nodo->next = NULL;
-    }
-    friend ostream& operator<<(ostream& os, const List& l) {
-        Node* temp = l.head;
-        while (temp != NULL) {
-            os << temp->data << " " << endl;
-            temp = temp->next;
-        }
-        return os;
-    }
-    List solucion(int k){
-        List l1;
-        List l2;
-        Node* temporal = head;
-        while (temporal != NULL) {
-            if(temporal->data < k){
-                  l1.pushBack(temporal->data);
-            }
-            else{
-                l2.pushBack(temporal->data);
-            }
-            temporal = temporal->next;
-        }
-        List l3;
-        Node* temporal2 = l1.getHead();
-        Node* temporal3 = l2.getHead();
-        while(temporal2 != NULL){
-            l3.pushBack(temporal2->data);
-            temporal2 = temporal2->next;
-        }
-        while(temporal3 != NULL){
-            l3.pushBack(temporal3->data);
-            temporal3 = temporal3->next;
-        }
-        return l3;
-    }
-};
-
-/* sol tarea
-    void pushFront(int numerito) {
-        Node* nuevoNode = new Node(numerito);
-        nuevoNode->next = head;
-        head = nuevoNode;
-    }
-    void pushBack(int numerito) {
-        Node* nodo = new Node(numerito);
-        if (!head) { head = nodo; return; }
-        Node* temp = head;
-        while (temp->next != NULL) {
-            temp = temp->next;
-        }
-        temp->next = nodo;
-        nodo->next = NULL;
-    }
-    void popFront() {
-        if (!head) return;
-        if (head->next == NULL) {
-            delete head;
-            head = NULL;
-        }
-        Node* temp = head;
-        head = head->next;
-        delete temp;
-    }
-    void popBack() {
-        Node *temp = head;
-        if (!head) return;
-        if (!head->next) { delete head; head = nullptr; return; }
-        while (temp->next->next != NULL) {
-            temp = temp->next;
-        }
-        delete temp->next;
-        temp->next = NULL;
-    }
-    void insert (int pos, int num) {
-        Node * nodo = new Node(num);
-        Node* temp = head;
-        int i = 0;
-        while (i++  < pos -1) {
-            temp = temp->next;
-        }
-        nodo->next = temp->next;
-        temp->next = nodo;
-    }
-    friend ostream& operator<<(ostream& os, const List& l) {
-        Node* temp = l.head;
-        while (temp != NULL) {
-            os << temp->data << " " << endl;
-            temp = temp->next;
-        }
-        return os;
+    Node* getHead() {
+        return head;
     }
 
-    void clear() {
-        while (head != NULL) {
-            Node* temp = head;
-            head = head->next;
-            delete temp;
-        }
-    }
     int front() {
         return head->data;
     }
@@ -140,6 +34,46 @@ class List {
         }
         return temp->data;
     }
+
+    void pushFront(int numerito) {
+        Node* nuevoNode = new Node(numerito);
+        nuevoNode->next = head;
+        head = nuevoNode;
+    }
+
+    void pushBack(int numerito) {
+        Node* nodo = new Node(numerito);
+        if (!head) { head = nodo; return; }
+        Node* temp = head;
+        while (temp->next != NULL) {
+            temp = temp->next;
+        }
+        temp->next = nodo;
+        nodo->next = NULL;
+    }
+
+    void popFront() {
+        if (!head) return;
+        if (head->next == NULL) {
+            delete head;
+            head = NULL;
+        }
+        Node* temp = head;
+        head = head->next;
+        delete temp;
+    }
+
+    void popBack() {
+        Node *temp = head;
+        if (!head) return;
+        if (!head->next) { delete head; head = nullptr; return; }
+        while (temp->next->next != NULL) {
+            temp = temp->next;
+        }
+        delete temp->next;
+        temp->next = NULL;
+    }
+
     int operator[](int pos) {
         Node* temp = head;
         int i = 0;
@@ -148,9 +82,11 @@ class List {
         }
         return temp->data;
     }
+
     bool empty() {
         return head == nullptr;
     }
+
     int size() {
         Node* temp = head;
         int i = 0;
@@ -161,6 +97,63 @@ class List {
         return i;
     }
 
+    void clear() {
+        while (head != NULL) {
+            Node* temp = head;
+            head = head->next;
+            delete temp;
+        }
+    }
+
+    void sort() {
+        if (!head || !head->next) return;
+        while (isOdered() == false) {
+            Node* t = head;
+            int contador = 0;
+            while ( t != NULL && t->next != NULL ) {
+                if (t->data > t->next->data) {
+                    swamp(contador);
+                }
+                t = t->next;
+                contador++;
+            }
+        }
+    }
+
+    void reverse() {
+        Node* prev = nullptr;
+        Node* curr = head;
+        Node* next = nullptr;
+
+        while (curr != nullptr) {
+            next = curr->next;
+            curr->next = prev;
+            prev = curr;
+            curr = next;
+        }
+
+        head = prev;
+    }
+
+    void insert (int pos, int num) {
+        Node * nodo = new Node(num);
+        Node* temp = head;
+        int i = 0;
+        while (i++  < pos -1) {
+            temp = temp->next;
+        }
+        nodo->next = temp->next;
+        temp->next = nodo;
+    }
+
+    friend ostream& operator<<(ostream& os, const List& l) {
+        Node* temp = l.head;
+        while (temp != NULL) {
+            os << temp->data << " " << endl;
+            temp = temp->next;
+        }
+        return os;
+    }
 
     void swamp(int i) {
         int si = size();
@@ -201,73 +194,39 @@ class List {
         return true;
     }
 
-    void sort() {
-        if (!head || !head->next) return;
-        while (isOdered() == false) {
-            Node* t = head;
-            int contador = 0;
-            while ( t != NULL && t->next != NULL ) {
-                if (t->data > t->next->data) {
-                    swamp(contador);
-                }
-                t = t->next;
-                contador++;
-            }
-        }
-    }
-    void reverse() {
-        Node* prev = nullptr;
-        Node* curr = head;
-        Node* next = nullptr;
-
-        while (curr != nullptr) {
-            next = curr->next;
-            curr->next = prev;
-            prev = curr;
-            curr = next;
-        }
-
-        head = prev;
-    }
-
     ~List() = default;
 
-};
+    };
 
-bool f() {
-    return true;
-}
-*/
+    bool f() {
+        return true;
+    }
 
 int main () {
-/* Tarea del profesor para subir a canvas
-    List l1;
-    cout << 5/2;
-    l1.pushFront(30);
-    l1.pushBack(50);
-    l1.pushFront(20);
-    l1.pushFront(10);
-    l1.pushBack(40);
+        List l1;
+        l1.pushFront(30);
+        l1.pushBack(50);
+        l1.pushFront(20);
+        l1.pushFront(10);
+        l1.pushBack(40);
 
-    cout << "-------------"<<endl;
+        cout << "-------------" << endl;
+
+        cout << l1;
+        l1.sort();
+        cout << "Order: ---" << endl;
+        cout << l1.isOdered() << endl;
+        cout << "Tamaño de lista: " << l1.size() << endl;
+
+        cout << "El valor en la posicion 2:" << endl;
+        cout << l1[2] << endl;
 
 
-    cout << l1;
-    l1.sort();
-    cout << l1.isOdered()<<endl;
-    l1.reverse();
-    cout << l1;
-*/
-    List milista;
-    int k = 3;
-    milista.pushBack(1);
-    milista.pushBack(4);
-    milista.pushBack(3);
-    milista.pushBack(2);
-    milista.pushBack(5);
-    milista.pushBack(2);
-    cout << milista << endl;
-    cout << "-----" << endl;
-    cout << milista.solucion(k) << endl;
+
+
+        l1.reverse();
+        cout << "Reverse: ---" << endl;
+        cout << l1;
+
 
 }
